e4119862c756567300db92339cc2fa74cd4da5fc
Himel <nazmulhasanhimel@yahoo.com>
2019-03-29 16:04:42 +0600

    mul table

---
+++ Movie_Rental/Movie_Rental/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Movie_Rental.Models;
+
+namespace Movie_Rental.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult table(int id)
+        {
+            var a = new Movie() { Id = id };   
+            return View(a);
+        }
+    }
+}
